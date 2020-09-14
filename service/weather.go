ffae353a8a5d452e318d54b9fb952f7cf079591c
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2020-09-14 21:57:50 +0300

    Return icon as string instead of rune/char int representation

--- service/weather.go
+++ service/weather.go
@@ -27,7 +27,7 @@ type Forecast struct {
 	WindSpeed      int         `json:"wind"`
 	WindDirection  int         `json:"direction"`
 	ConditionCode  string      `json:"condition"`
-	Icon           rune        `json:"icon"`
+	Icon           string      `json:"icon"`
 }
 
 type Weather interface {
@@ -84,7 +84,7 @@ func mapFromMeteoltResponse(data []meteolt.Forecast) []Forecast {
 		ConditionCode:  data[0].ConditionCode,
 		WindDirection:  data[0].WindDirection,
 		WindSpeed:      data[0].WindSpeed,
-		Icon:           data[0].Icon,
+		Icon:           string(data[0].Icon),
 	}
 
 	// TODO: implement mapping for remaining days
